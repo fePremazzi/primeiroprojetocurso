16743cb70dde2a853059f75a588dda1fbf33a8b8
Fellipe Premazzi Rego <felliperego@hotmail.com>
2018-12-04 20:39:44 -0200

    added change text button and clean text button, end of the project

--- app/src/main/java/com/example/a212697748/primeiroprojetocurso/MainActivity.kt
+++ app/src/main/java/com/example/a212697748/primeiroprojetocurso/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.a212697748.primeiroprojetocurso
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun buttonClick( view: View){
+        Log.i("BOTAO", "Botão foi pressionado");
+
+        //var texto = findViewById(R.id.txtExibicao) as TextView
+        var texto = findViewById<TextView>(R.id.txtExibicao)
+        texto.setText("Eita carai, funcionou!")
+    }
+
+    fun buttonLimparClick(view: View){
+        var txt = findViewById<TextView>(R.id.txtExibicao)
+        txt.setText("Clique no botão")
+    }
+
 }
